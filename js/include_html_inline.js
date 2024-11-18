44281e65f749d2ed3e1a0194c8efcfd0788de955
Joey <joey.markowitz@gmail.com>
2024-11-18 02:12:57 -0500

    CLEANUP - js code and old images

--- js/include_html_inline.js
+++ js/include_html_inline.js
@@ -1,12 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', html_template_insert);
+
+function html_template_insert() {
+    /*
+    // Inserts HTML content from a template file into elements with the 'w3-include-html' attribute.
+    // 
+    // This function finds all elements with the 'w3-include-html' attribute, fetches the HTML content from the 
+    // specified file URL, and inserts that content into the element. Once the content is loaded and inserted, 
+    // the 'w3-include-html' attribute is removed to prevent multiple inserts.
+    //
+    //
+    // EXAMPLE usage
+    //
+    // HTML code
+    // <div w3-include-html="social_links.html" id="social_links" class="social_links"></div>
+    //
+    // OUTCOME
+    // in the above case, populate the DIV w socail links from the file social_links.html
+    // This allows changes to be made to the source file only, not every reference in many pages
+    */
     var elements = document.querySelectorAll('[w3-include-html]');
     elements.forEach(function(elmnt) {
+        // Load the specified include file
         var file = elmnt.getAttribute('w3-include-html');
+
         if (file) {
             fetch(file)
                 .then(response => response.text())
                 .then(data => {
+                    // insert the template file content into the element (DIV) text()
                     elmnt.innerHTML = data;
+                    // remove all trace of the attribute so multiple inserts do not occur
                     elmnt.removeAttribute('w3-include-html');
                 })
                 .catch(error => {
@@ -15,25 +38,4 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
         }
     });
-});
-
-// function includeHTML() {
-//     var elements = document.querySelectorAll('[w3-include-html]');
-//     elements.forEach(function(elmnt) {
-//         var file = elmnt.getAttribute('w3-include-html');
-//         if (file) {
-//             fetch(file)
-//                 .then(response => response.text())
-//                 .then(data => {
-//                     elmnt.innerHTML = data;
-//                     elmnt.removeAttribute('w3-include-html');
-//                 })
-//                 .catch(error => {
-//                     console.error('Error loading file:', error);
-//                     elmnt.innerHTML = "Page not found.";
-//                 });
-//         }
-//     });
-// }
-
-// window.onload = includeHTML;
+}
