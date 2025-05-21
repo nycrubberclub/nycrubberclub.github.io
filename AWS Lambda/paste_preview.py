e676ec56981c001d20b865f3aebdae3506e92819
Joey <joey.markowitz@gmail.com>
2025-05-20 23:11:05 -0400

    UPDATE - add error checking to code

--- AWS Lambda/paste_preview.py
+++ AWS Lambda/paste_preview.py
@@ -18,21 +18,31 @@ def lambda_handler(event, context):
 
     # Find the valid image entry
     valid_entry = None
-    for entry in schedule_response.json()['events']:
-        startdate_utc = datetime.strptime(entry["datetime_valid_et"], datetime_format).astimezone(timezone.utc)
-        enddate_utc = datetime.strptime(entry["datetime_expire_et"], datetime_format).astimezone(timezone.utc)
-        now = datetime.now(timezone.utc)
+    for entry in schedule_response.json().get('events', []):
+        try:
+            now = datetime.now(timezone.utc)
+            startdate_utc = datetime.strptime(entry["datetime_valid_et"], datetime_format).astimezone(timezone.utc)
+            enddate_utc = datetime.strptime(entry["datetime_expire_et"], datetime_format).astimezone(timezone.utc)
 
-        if startdate_utc <= now < enddate_utc:
-            valid_entry = entry
-            break
+            if startdate_utc <= now < enddate_utc:
+                valid_entry = entry
+                print(f"[DEBUG] Found valid entry: {entry}")
+                break
+                
+        except Exception as e:
+            print(f"[DEBUG] Failed to parse event source date.  ({entry})")
+            print(f"[ERROR] Exception: {e}")
+
+            continue
 
     if valid_entry:
         image_url = domain_url + valid_entry["url_image"]
 
         # Fetch the image content
         image_response = requests.get(image_url)
-        if image_response.status_code != 200:   
+        if image_response.status_code != 200:
+            print("[DEBUG] Failed to fetch event image.")
+ 
             return {
                 'statusCode': 400,
                 'body': 'Failed to fetch event image'
@@ -52,12 +62,9 @@ def lambda_handler(event, context):
             }
 
     else:
+        print("[DEBUG] No valid event found, using default image.")
+
         image_response = requests.get(image_url_default)
-        if image_response.status_code != 200:   
-            return {
-                'statusCode': 400,
-                'body': 'Failed to fetch default image'
-            }
 
         if image_response.status_code == 200:
             encoded_image = base64.b64encode(image_response.content).decode('utf-8')
@@ -71,8 +78,14 @@ def lambda_handler(event, context):
                 "body": encoded_image, #image_response.content,
                 "isBase64Encoded": True  # Encode for binary image content
             }
-    
+        else:
+            print("[DEBUG] Failed to fetch default image.")
 
+            return {
+                'statusCode': 400,
+                'body': 'Failed to fetch default image'
+            }
+    
 
     # No valid image found
     return {
